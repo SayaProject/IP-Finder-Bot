d25a8ab604c10124f73706adf49b1de203397948
SHNWAZ <271950542+shnwazdeveloper@users.noreply.github.com>
2026-06-25 10:33:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import path, getenv
 
 class config:
-    API_ID = int(getenv("API_ID", "Enter Your API KEY"))
-    API_HASH = getenv("API_HASH", "Enter Your API Hash")
+    API_ID = int(getenv("API_ID", "30422005"))
+    API_HASH = getenv("API_HASH", "5170ded206641d73215baf40175a6924")
     BOT_TOKEN = getenv("BOT_TOKEN", "Enter Your BOT Token")
-    IP_API =getenv("ACCESS_TOKEN","IP-API API KEY IS HERE")
+    IP_API =getenv("ACCESS_TOKEN","513fceccaba807")
     
 con = config()
